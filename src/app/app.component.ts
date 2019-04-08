ac457c525ca5f54164ede497fb8d24b87e01e3a0
sreekumar <sreekumar456@gmail.com>
2019-04-09 02:11:08 +0800

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Message } from '@app/models';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public message : Message;
+  public messages : Message[];
+
+
+  constructor(){
+    this.message = new Message('', 'assets/images/user.png');
+    this.messages = [
+      new Message('Welcome to Manulife', 'assets/images/boot.jpg', new Date())
+    ];
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  token: 'f12736b4732a4beb91f39c6c3b9447f9'
+};
